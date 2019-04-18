8d53cdcc7630e70185d2782c0023e2903f8c472e
Doombringer <royalthings2019@gmail.com>
2019-04-18 09:35:12 +0300

    webView

--- GitHubApiAlamofire/DetailViewController.swift
+++ GitHubApiAlamofire/DetailViewController.swift
@@ -9,23 +9,66 @@
 import UIKit
 import WebKit
 
-class DetailViewController: UIViewController, UIWebViewDelegate {
+class DetailViewController: UIViewController, WKNavigationDelegate {
 
     
     @IBOutlet weak var webView: WKWebView!
     var srtURL = ""
     
     
+    @IBOutlet weak var goBackItem: UIBarButtonItem!
+    @IBOutlet weak var goForwardItem: UIBarButtonItem!
+    
+    let application = UIApplication.shared
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webView.navigationDelegate = self
 
-        let url = URL(string: srtURL)
-        let request = URLRequest(url: url!)
-        webView.load(request)
+        if let url = URL(string: srtURL) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        
         
     }
     
+    //MARK: - WKNavigationDelegate
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        application.isNetworkActivityIndicatorVisible = true
+        self.goBackItem.isEnabled = false
+        self.goForwardItem.isEnabled = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        application.isNetworkActivityIndicatorVisible = false
+        if webView.canGoBack {
+            self.goBackItem.isEnabled = true
+        } else if webView.canGoForward {
+            self.goForwardItem.isEnabled = true
+        }
+    }
+    
+    
+    @IBAction func goBackAction(_ sender: Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
 
-
+    @IBAction func goForwardAction(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func refreshAction(_ sender: Any) {
+        webView.reload()
+    }
+    
 
 }
